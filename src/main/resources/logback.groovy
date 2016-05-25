883d15ef19da259396a7bc16ac9df590e8add015
phuong <phuonghqh@gmail.com>
2016-05-25 11:04:32 +0700

    Refactor logback

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,4 +1,4 @@
-package staging
+package com.vnw.data
 
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 import ch.qos.logback.core.ConsoleAppender
@@ -6,6 +6,7 @@ import ch.qos.logback.core.rolling.RollingFileAppender
 import ch.qos.logback.core.util.FileSize
 
 import static ch.qos.logback.classic.Level.DEBUG
+import static ch.qos.logback.classic.Level.ERROR
 
 scan()
 
@@ -13,31 +14,24 @@ def LOG_FOLDER = "./"
 
 new File(LOG_FOLDER).mkdirs()
 
-appender("FILE", RollingFileAppender) {
-  file = "${LOG_FOLDER}dtool.log"
-  rollingPolicy(SizeAndTimeBasedRollingPolicy) {
-    fileNamePattern = "${LOG_FOLDER}dtool-%d{yyyyMMdd}.%i.log"
-    maxHistory = 10
-    maxFileSize = "10mb"
-    totalSizeCap = FileSize.valueOf("1gb")
-  }
-  encoder(PatternLayoutEncoder) {
-    pattern = "%d{HH:mm:ss.SSS} %p [%t] %c{1}: %m%n"
+def fileLogger = { String appenderName, String fileName ->
+  appender("${appenderName}", RollingFileAppender) {
+    file = "${LOG_FOLDER}/${fileName}.log"
+    rollingPolicy(SizeAndTimeBasedRollingPolicy) {
+      fileNamePattern = "${LOG_FOLDER}/${fileName}.%d{yyyyMMdd}.%i.log"
+      maxHistory = 10
+      maxFileSize = "10mb"
+      totalSizeCap = FileSize.valueOf("1gb")
+    }
+    encoder(PatternLayoutEncoder) {
+      pattern = "%d{HH:mm:ss.SSS} %p [%t] %c{1}: %m%n"
+    }
   }
 }
 
-appender("DEBUG", RollingFileAppender) {
-  file = "${LOG_FOLDER}dtool-debug.log"
-  rollingPolicy(SizeAndTimeBasedRollingPolicy) {
-    fileNamePattern = "${LOG_FOLDER}dtool-debug-%d{yyyyMMdd}.%i.log"
-    maxHistory = 10
-    maxFileSize = "10mb"
-    totalSizeCap = FileSize.valueOf("1gb")
-  }
-  encoder(PatternLayoutEncoder) {
-    pattern = "%d{HH:mm:ss.SSS} %p [%t] %c{1}: %m%n"
-  }
-}
+fileLogger "FILE", "dtool"
+fileLogger "DEBUG", "dtool-debug"
+fileLogger "JOOQ", "dtool-jooq"
 
 appender("CONSOLE", ConsoleAppender) {
   encoder(PatternLayoutEncoder) {
@@ -46,6 +40,8 @@ appender("CONSOLE", ConsoleAppender) {
 }
 
 logger("com.vnw.data.service", DEBUG, ["CONSOLE", "DEBUG"])
+logger("com.vnw.data.repo", DEBUG, ["CONSOLE", "DEBUG"])
+logger("org.jooq", DEBUG, ["CONSOLE", "JOOQ"])
 
-root(DEBUG, ["CONSOLE", "FILE"])
+root(ERROR, ["CONSOLE", "FILE"])
 
